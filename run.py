a573d5c0909d9a3416ac6a25d27ecc2be142e360
Makima3 <natalia.kolchuk@gmail.com>
2024-07-19 23:18:34 +0300

    init commit

---
+++ run.py
@@ -0,0 +1,25 @@
+import os
+import asyncio
+from dotenv import load_dotenv
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+load_dotenv()
+TOKEN = os.environ.get("TOKEN")
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def cmd_start(message: Message) -> None:
+    await message.answer("Hello!")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
