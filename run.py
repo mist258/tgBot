c024e84245485fdc85235b434f6a96102e4793a9
Makima3 <natalia.kolchuk@gmail.com>
2024-07-20 23:20:19 +0300

    init commit

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.types import Message, reaction_type
 
 load_dotenv()
 TOKEN = os.environ.get("TOKEN")
@@ -14,7 +14,7 @@ dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def cmd_start(message: Message) -> None:
-    await message.answer("Hello!")
+    await message.reply("Hello!")
 
 
 async def main():
