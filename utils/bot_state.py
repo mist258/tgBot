c7f093c9acf20ffa7db4b75e7a7cb6f986b60c05
Makima3 <natalia.kolchuk@gmail.com>
2024-07-28 13:48:52 +0300

    add time & user id

--- utils/bot_state.py
+++ utils/bot_state.py
@@ -2,9 +2,24 @@ from aiogram.fsm.state import StatesGroup, State
 from aiogram import Router, F
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
+import json
+import re
+import datetime
 
 
 router_state = Router()
+
+
+def validate_input(mssg) -> bool:  # validate  user input (int | float)
+    pattern = re.compile(r'^-?\d+(\.\d+)?$')
+    if pattern.fullmatch(mssg):
+        return True
+    return False
+
+
+# def time_data():
+#     data_time = datetime.datetime.now().strftime("%Y-%m-%d")
+
 
 '''state for income'''
 
@@ -20,11 +35,14 @@ async def add_income(message: Message, state: FSMContext) -> None:
 
 
 @router_state.message(IncomeStates.income)
-async def general_income(message: Message, state: FSMContext) -> None:
-    if message.text.isdigit():
-        await state.update_data(general_income=message.text)
+async def general_income(message: Message, state: FSMContext,) -> None:
+    data_time = datetime.datetime.now().strftime("%Y-%m-%d")
+    if validate_input(message.text):
+        await state.update_data(general_income=message.text, duration=data_time, id=message.from_user.id)
         data = await state.get_data()
-        await message.answer(f'Your income: {data["general_income"]} $')
+        await message.answer(f'Your income: {data["general_income"]}$')
+        with open('data.json', 'w') as f:
+            json.dump(data, f)
         await state.clear()
     else:
         await message.answer('Enter only integer or float.')
@@ -45,10 +63,13 @@ async def add_rent_expense(message: Message, state: FSMContext) -> None:
 
 @router_state.message(RentState.rent_expense)
 async def get_rent_expense(message: Message, state: FSMContext) -> None:
-    if message.text.isdigit():
-        await state.update_data(rent_expense=message.text)
+    data_time = datetime.datetime.now().strftime("%Y-%m-%d")
+    if validate_input(message.text):
+        await state.update_data(rent_expense=message.text, duration=data_time, id=message.from_user.id)
         data = await state.get_data()
         await message.answer(f'Your rent expense: {data["rent_expense"]} $')
+        with open('data.json', 'w') as f:
+            json.dump(data, f)
         await state.clear()
     else:
         await message.answer('Enter only integer or float.')
@@ -67,10 +88,13 @@ async def add_food_expense(message: Message, state: FSMContext) -> None:
 
 @router_state.message(FoodState.food_expense)
 async def get_food_expense(message: Message, state: FSMContext) -> None:
-    if message.text.isdigit():
-        await state.update_data(food_expense=message.text)
+    data_time = datetime.datetime.now().strftime("%Y-%m-%d")
+    if validate_input(message.text):
+        await state.update_data(food_expense=message.text, duration=data_time, id=message.from_user.id)
         data = await state.get_data()
         await message.answer(f'Your food expense: {data['food_expense']}$')
+        with open('data.json', 'w') as f:
+            json.dump(data, f)
         await state.clear()
     else:
         await message.answer('Enter only integer or float.')
@@ -89,10 +113,13 @@ async def add_clothes_expense(message: Message, state: FSMContext) -> None:
 
 @router_state.message(ClothesState.clothes_expense)
 async def get_clothes_expense(message: Message, state: FSMContext) -> None:
-    if message.text.isdigit():
-        await state.update_data(clothes_expense=message.text)
+    data_time = datetime.datetime.now().strftime("%Y-%m-%d")
+    if validate_input(message.text):
+        await state.update_data(clothes_expense=message.text, duration=data_time, id=message.from_user.id)
         data = await state.get_data()
         await message.answer(f'Your clothes expense: {data["clothes_expense"]} $')
+        with open('data.json', 'w') as f:
+            json.dump(data, f)
         await state.clear()
     else:
         await message.answer('Enter only integer or float.')
@@ -111,10 +138,13 @@ async def add_gym_expense(message: Message, state: FSMContext) -> None:
 
 @router_state.message(GymState.gym_expense)
 async def get_gym_expense(message: Message, state: FSMContext) -> None:
-    if message.text.isdigit():
-        await state.update_data(gym_expense=message.text)
+    data_time = datetime.datetime.now().strftime("%Y-%m-%d")
+    if validate_input(message.text):
+        await state.update_data(gym_expense=message.text, duration=data_time, id=message.from_user.id)
         data = await state.get_data()
-        await message.answer(f'Your gym expense: {data["gym_expense"]} $')
+        await message.answer(f'Your gym expense: {data["gym_expense"]}$')
+        with open('data.json', 'w') as f:
+            json.dump(data, f)
         await state.clear()
     else:
         await message.answer('Enter only integer or float.')
@@ -133,10 +163,13 @@ async def add_medicine_expense(message: Message, state: FSMContext) -> None:
 
 @router_state.message(MedicineState.medicine_expense)
 async def get_medicine_state(message: Message, state: FSMContext) -> None:
-    if message.text.isdigit():
-        await state.update_data(medicine_expense=message.text)
+    data_time = datetime.datetime.now().strftime("%Y-%m-%d")
+    if validate_input(message.text):
+        await state.update_data(medicine_expense=message.text, duration=data_time, id=message.from_user.id)
         data = await state.get_data()
         await message.answer(f'Your medicine expense: {data["medicine_expense"]} $')
+        with open('data.json', 'w') as f:
+            json.dump(data, f)
         await state.clear()
     else:
         await message.answer('Enter only integer or float.')
@@ -155,10 +188,13 @@ async def add_other_expense(message: Message, state: FSMContext) -> None:
 
 @router_state.message(OtherState.other_expense)
 async def get_other_expense(message: Message, state: FSMContext) -> None:
-    if message.text.isdigit():
-        await state.update_data(other_expense=message.text)
+    data_time = datetime.datetime.now().strftime("%Y-%m-%d")
+    if validate_input(message.text):
+        await state.update_data(other_expense=message.text, duration=data_time, id=message.from_user.id)
         data = await state.get_data()
         await message.answer(f'Your other expense: {data["other_expense"]} $')
+        with open('data.json', 'w') as f:
+            json.dump(data, f)
         await state.clear()
     else:
         await message.answer('Enter only integer or float.')
