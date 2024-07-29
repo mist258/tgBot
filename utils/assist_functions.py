e9cf895bb766c6ad79022e4336d4254f5f61bff1
Makima3 <natalia.kolchuk@gmail.com>
2024-07-29 23:39:39 +0300

    add function for record & read data

--- utils/assist_functions.py
+++ utils/assist_functions.py
@@ -1,3 +1,4 @@
+import json
 import re
 
 
@@ -8,15 +9,30 @@ def validate_input(mssg) -> bool:  # validate  user input (int | float)
     return False
 
 
-class FileHandler:
+class FileHandler:  # context manager's class
     def __init__(self, filename, mode):
         self.filename = filename
         self.mode = mode
-        self.file = None
 
     def __enter__(self):
-        pass
+        self.file = open(self.filename. self.mode)
+        return self.file
 
     def __exit__(self, ext_type, exc_value, traceback):
         self.file.close()
 
+
+def read_data(filename):  # for reading recorded data
+    try:
+        with FileHandler(filename, 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return {}
+
+
+def write_data(filename, data):  # for recording updated data
+    try:
+        with FileHandler(filename, 'w') as f:
+            return json.dump(data,f, indent=4)
+    except FileNotFoundError:
+        return {}
